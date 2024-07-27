38d4bf648e3dc99e0d0c790d980048f205ecafe8
Mike <mikewise1618@gmail.com>
2024-07-27 15:26:58 +0200

    changes to get to compile with jax 0.4.30

---
+++ minjax.py
@@ -0,0 +1,7 @@
+from jax.lax import conv
+import jax.random as jrd
+
+rng = jrd.PRNGKey(0)
+x = jrd.truncated_normal(rng, -1, 1, (1, 1, 28, 28))
+kernel = jrd.truncated_normal(rng, -1, 1, (2, 1, 3, 3))
+y = conv(x, kernel, window_strides=(1, 1), padding='SAME')
---
+++ simpletest.py
@@ -0,0 +1,5 @@
+import jax 
+import jax.numpy as jnp 
+n = 1 
+l = 0
+T = (2*l+1)*jnp.pi/2*(jnp.sqrt(n+1)+jnp.sqrt(n))
